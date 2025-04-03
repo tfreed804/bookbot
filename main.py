c6fac314c2661ad8aac789b8fb89545c81559305
tfreed804 <tfreed804@gmail.com>
2025-04-03 18:01:11 -0400

    2. Added the sort_on() and final_report() functions to stats.py and updated the main() function in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #import functions from stats.py
 from stats import word_count
 from stats import character_count
+from stats import final_report
 
 #This function will open the .txt file of a book and save its contents as a string
 def get_book_text(file_path):
@@ -15,9 +16,18 @@ def main():
     text = get_book_text(file_path)
     num_words = word_count(text)
     num_chars = character_count(text)
+    sorted_chars = final_report(num_chars)
     
-    print(f"{num_words} words found in the document")
-    print(character_count(text))
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for char_dict in sorted_chars:
+        char = char_dict["char"]
+        count = char_dict["count"]
+        print(f"{char}: {count}")
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -11,5 +11,17 @@ def character_count(book_text):
     for char in book_text:
         if char.isprintable():
             count_dict[char] = count_dict.get(char, 0) + 1
-    
+
     return count_dict
+
+#This function will allow me to sort the new dictionary below
+def sort_on(dict):
+    return dict["count"]
+
+#this function will create a list of dictionaries from the original count_dict dictionary
+#in the character_count function.
+def final_report(count_dict):
+    list_of_dicts = [{"char": k, "count": v} for k, v in count_dict.items() if k.isalpha()]
+    list_of_dicts.sort(reverse=True, key=sort_on)
+    
+    return list_of_dicts
